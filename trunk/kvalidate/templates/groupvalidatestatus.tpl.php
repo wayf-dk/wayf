beba875c1b48f467a74a4f53cb5e37e9e4e7ec83
jach@wayf.dk <jach@wayf.dk@f0b3050b-8f52-9014-a1fc-77610a68c25a>
2010-09-13 08:48:16 +0000

    [Kvalidate] Moved style information from groupvalidate to seperat file

--- trunk/kvalidate/templates/groupvalidatestatus.tpl.php
+++ trunk/kvalidate/templates/groupvalidatestatus.tpl.php
@@ -28,46 +28,18 @@
  
 $this->data['header'] = $this->t('{kvalidate:kvalidate:title}');
 
-$this->data['head'] = '
-<style type="text/css">
-	table {
-		border-collapse: collapse;
-		margin: 0px 0px 20px;
-		padding: 0px;
-		border-spacing: 2px 2px;
-		border-color: gray;
-		display: table;
-	}
-	td {
-		border: 1px solid #5F911B;
-		padding: 3px;
-		margin: 0px;
-		display: table-cell;
-		vertical-align: inherit;
-	}
-	th {
-		border: 1px solid #5F911B;
-		background: #7FA748;
-		color: white;
-		padding: 3px;
-		margin: 0px;
-		font-weight: bold;
-		display: table-cell;
-		vertical-align: inherit;
-	}
-</style>
-';
+$this->data['head'] = '<link rel="stylesheet" type="text/css" href="resources/style.css" />';
 
 $this->includeAtTemplateBase('includes/header.php');
 
 echo '<h1>' . $this->t('{kvalidate:kvalidate:groupvalidation_header}') . ' - ' . $this->data['group'] . '</h1>';
 
 if(isset($this->data['error'])) {
-    echo '<br /><p style="color: #FF0000; font-weight: bold;">' . $this->data['error'] . '</p>';
+    echo '<br /><p class="group_error">' . $this->data['error'] . '</p>';
 } else {
     echo '<p>' . $this->t('{kvalidate:kvalidate:see_group_status}') . '</p>';
     echo '
-        <table style="width: 300px;">
+        <table class="group_table">
         <tr>
         <th>' . $this->t('{kvalidate:kvalidate:status}') . '</th>
         <th>' . $this->t('{kvalidate:kvalidate:entity}') . '</th>
