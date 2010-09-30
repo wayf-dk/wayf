f68cf7f8ebbe0fc3bbf81744f7a12ac494cc2a70
jach@wayf.dk <jach@wayf.dk@f0b3050b-8f52-9014-a1fc-77610a68c25a>
2010-09-30 10:41:49 +0000

    [kvalidate] Tagged Kvalidate version 0.2.2

---
+++ tags/kvalidate/v.0.2.2/www/groupvalidate.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Kalmar2 metadata validator
+ *
+ * PHP version 5
+ *
+ * The Kalmar2 metadata validator is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The Kalmar2 metadata validator is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should haveKalmar2 metadata validator. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @package    SimpleSAMLphp
+ * @subpackege Module
+ * @author     Jacob Christiansen <jach@wayf.dk>
+ * @copyright  2010 Jacob Christiansen 
+ * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @version    SVN: $Id$
+ * @link       http://code.google.com/p/wayf/
+ */
+
+$kv_config = SimpleSAML_Configuration::getConfig('module_kvalidate.php');
+$globalConfig = SimpleSAML_Configuration::getConfig();
+
+if(isset($_GET['tag'])) {
+	$tags = $kv_config->getArray('tags');
+	if(isset($tags[$_GET['tag']])) {
+		$tag = $tags[$_GET['tag']];
+	} else {
+		$error = 'Tag not defined';
+	}
+} else {
+    $_GET['tag'] = '';
+	$error = 'No tag given';
+}
+
+$t = new SimpleSAML_XHTML_Template($globalConfig, 'kvalidate:groupvalidatestatus.tpl.php');
+
+$t->data['group'] = htmlentities($_GET['tag']);
+
+if(isset($error)) {
+    $t->data['error'] = $error;
+    $t->show();
+    exit;
+}
+
+$t->data['entities'] = array();
+	
+$config['REMOVE_ENTITYDESCRIPTOR'] = $kv_config->getBoolean('remove.entitydescriptor', false);
+	
+$validator = new sspmod_kvalidate_Validator($config);
+
+ksort($tag);
+
+foreach($tag AS $k => $entity) {
+    /**
+     * Can not be used because of bug in FILTER_VALIDATE_URL 
+     * (http://bugs.php.net/bug.php?id=51192). Bug should be fixed in PHP 
+     * 5.3.3/5.2.14
+     */
+    //$md_url = filter_var($entity['url'], FILTER_VALIDATE_URL);
+	$md_url = $entity['url'];
+	$xml = file_get_contents($md_url);
+	$t->data['entities'][$k]['xml'] = $validator->validate($xml);
+	$t->data['entities'][$k]['messages'] = $validator->getMessages();
+	$t->data['entities'][$k]['status'] = $validator->getStatus();
+	$t->data['entities'][$k]['name'] = $entity['name'];
+	$t->data['entities'][$k]['description'] = $entity['description'];
+	$t->data['entities'][$k]['url'] = $md_url;
+}
+
+$t->show();
+exit;
