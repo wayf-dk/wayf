88973ccb483ed8d4b7c795e040dfb1548b930a08
jach@wayf.dk <jach@wayf.dk@f0b3050b-8f52-9014-a1fc-77610a68c25a>
2010-10-01 11:56:27 +0000

    [kvalidate] Merged fixes from v.0.2 branch to trunk

--- kvalidate/lib/Validator.php
+++ kvalidate/lib/Validator.php
@@ -555,7 +555,7 @@ class sspmod_kvalidate_Validator {
                         } else {
                             $this->_messages[] = array( 
                                 'level' => KV_STATUS_WARNING,
-                                'msg' => '[' . $input_elm->patentNode->getAttribute('entityID') . '] `use` attribute not given in `KeyDescriptor`. Should be set to `encryption`',
+                                'msg' => '[' . $input_elm->parentNode->getAttribute('entityID') . '] `use` attribute not given in `KeyDescriptor`. Should be set to `encryption`',
                                 'line' => $input_elm->getLineNo(),
                             );
                             $kd_found = true;
@@ -610,6 +610,13 @@ class sspmod_kvalidate_Validator {
                     );
                     return true; 
                 }
+            } else {
+                $this->_messages[] = array(
+                    'level' => KV_STATUS_WARNING,
+                    'msg' => '[' . $input_elm->parentNode->getAttribute('entityID') . '] `use` attribute not given in `KeyDescriptor`. Should be set to `signing`',
+                    'line' => $elm->getLineNo(),
+                );
+                return true;
             }
         }
         $this->_messages[] = array( 
