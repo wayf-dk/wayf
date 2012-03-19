76b28a90aec6ad2407add2789b20d06b6fcbd12f
jach@wayf.dk <jach@wayf.dk@f0b3050b-8f52-9014-a1fc-77610a68c25a>
2012-03-19 15:02:41 +0000

    New directory structure

--- bridgestat/www/config.php
+++ bridgestat/www/config.php
@@ -4,13 +4,13 @@ global $config;
 
 
 $config = array(
-		'requireAuth' => false,
+		'requireAuth' => true,
 		
 		//Database information
 		'dbHost' => '127.0.0.1',
-		'dbName' => 'logstat3',
-		'dbUser' => 'root',
-		'dbPwd' => 'sdflkj',
+		'dbName' => 'wayf_log',
+		'dbUser' => 'statuser',
+		'dbPwd' => 'password',
 		
 		//Maximum number of bins for histograms
 		'MAX_NUMBER_OF_BINS' => 10000,
