5389f8c3677a84d872ba1b50ee7471653d866af3
jach@wayf.dk <jach@wayf.dk@f0b3050b-8f52-9014-a1fc-77610a68c25a>
2012-03-21 12:16:02 +0000

    Cleanup

--- bridgestat/www/admin/index.php
+++ bridgestat/www/admin/index.php
@@ -27,7 +27,6 @@ else {
   internalError("You must set eid");
 }
 
-
 $dbConnection = dbConnect();
 
 $role = getRole($eppn, $entity);
@@ -58,70 +57,61 @@ mysql_close($dbConnection);
 ?>
 <html>
 <head>
-<script type = "text/javascript">
-  // Sets html input tags so a submit will delete the given eduPersonPrincipalName.
-  // And submit.
-  // INPUT
-  //   eppn - eduPersonPrincipalName (string)
-  function del(eppn) {
-    document.getElementById('action').value = 'del';
-    document.getElementById('arg').value = eppn;
-    document.form.submit();
-  }
-</script>
+    <script type = "text/javascript">
+        // Sets html input tags so a submit will delete the given eduPersonPrincipalName.
+        // And submit.
+        // INPUT
+        //   eppn - eduPersonPrincipalName (string)
+        function del(eppn) {
+            document.getElementById('action').value = 'del';
+            document.getElementById('arg').value = eppn;
+            document.form.submit();
+        }
+    </script>
 </head>
-
-
 <body>
-
-<form name ="form">
-<input type = 'hidden' name = 'eid' value = '<?echo $entity?>' />
-<input type = 'hidden' id = 'action' name = 'action' value = 'add' />
-<input type = 'hidden' id = 'arg' name = 'arg' value = '' />
-<table>
-
-<tr>
-<th>
-eduPersonPrincipalName
-</th>
-<th>
-Role
-</th>
-</tr>
-<?php
-for($i = 0; $i < count($delegated); $i++) {
-  $user = $delegated[$i];
-  echo "<tr>";
-  echo "<td>";
-  echo $user[0];
-  echo "</td>";
-  echo "<td>";
-  echo $user[1];
-  echo "</td>";
-  echo "<td>";
-  echo "<button type='button' onclick = 'del(\"".$user[0]."\");'>";
-  echo "Delete";
-  echo "</button>";
-  echo "</td>";
-  echo "</tr>";
-}
-?>
-<tr>
-<td>
-<input type = 'text' name = 'eppn' />
-</td>
-<td>
-<select name = 'role'>
-<option value = 'viewer'> viewer
-<option value = 'admin'> admin
-</select>
-</td>
-<td>
-<input type = "submit" value = 'Add'/>
-</td>
-</tr>
-</table>
-</form>
+    <form name ="form">
+        <input type = 'hidden' name = 'eid' value = '<?echo $entity?>' />
+        <input type = 'hidden' id = 'action' name = 'action' value = 'add' />
+        <input type = 'hidden' id = 'arg' name = 'arg' value = '' />
+        <table>
+            <tr>
+                <th>eduPersonPrincipalName</th>
+                <th>Role</th>
+            </tr>
+            <?php
+            for($i = 0; $i < count($delegated); $i++) {
+            $user = $delegated[$i];
+            echo "<tr>";
+            echo "<td>";
+            echo $user[0];
+            echo "</td>";
+            echo "<td>";
+            echo $user[1];
+            echo "</td>";
+            echo "<td>";
+            echo "<button type='button' onclick = 'del(\"".$user[0]."\");'>";
+            echo "Delete";
+            echo "</button>";
+            echo "</td>";
+            echo "</tr>";
+            }
+            ?>
+            <tr>
+                <td>
+                    <input type = 'text' name = 'eppn' />
+                </td>
+                <td>
+                    <select name = 'role'>
+                        <option value = 'viewer'> viewer
+                        <option value = 'admin'> admin
+                    </select>
+                </td>
+                <td>
+                    <input type = "submit" value = 'Add'/>
+                </td>
+            </tr>
+        </table>
+    </form>
 </body>
-
 </html>
