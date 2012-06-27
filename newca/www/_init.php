ac7ad214872ef901305f5f0fa06a1490f401f999
jach@wayf.dk <jach@wayf.dk@f0b3050b-8f52-9014-a1fc-77610a68c25a>
2012-06-27 09:41:39 +0000

    Added initialization script

---
+++ newca/www/_init.php
@@ -0,0 +1,96 @@
+<?php
+// Define the root path of NEWCA 
+define('ROOT', dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
+define('CONFIGROOT', ROOT . 'config' . DIRECTORY_SEPARATOR);
+define('LOGROOT', ROOT . 'log' . DIRECTORY_SEPARATOR);
+
+// System status
+define('STATUS', 'development');
+
+// Set error logging
+ini_set('log_errors', TRUE);
+ini_set('error_reporting', -1);
+
+// Disable magic_quotes_*
+ini_set('magic_quotes_gpc', 'off');
+ini_set('magic_quotes_runtime', 'off');
+ini_set('magic_quotes_sybase', 'off');
+
+// Check what status we have
+switch (STATUS) {
+    case 'production': {
+        ini_set('display_errors', 'off');
+        ini_set('display_startup_errors', FALSE);
+        break;
+    }
+    case 'development': {
+        // Display all errors in development
+        ini_set('display_errors', 'on');
+        ini_set('display_startup_errors', TRUE);
+        break;
+    }
+    default: {
+        die('Application status not set. Terminating execution.');
+    }
+}
+
+// Include the autoloader
+include ROOT . 'lib' . DIRECTORY_SEPARATOR . 'WAYF' . DIRECTORY_SEPARATOR . 'AutoLoader.php';
+
+// Register all classes under WAYF
+$classLoader = new \WAYF\AutoLoader('WAYF', ROOT . 'lib');
+$classLoader->register();
+
+
+
+/*
+$jakob_config = \WAYF\Configuration::getConfig();
+$template = new \WAYF\Template();
+try {
+    $logger = \WAYF\LoggerFactory::createInstance($jakob_config['logger']);
+} catch (\WAYF\LoggerException $e) {
+   $data = array('errortitle' => 'Logger could not be initiated', 'errormsg' => $e->getMessage());
+   $template->setTemplate('error')->setData($data)->render();
+}
+
+// Set exception handler
+$exceptionHandler = new \WAYF\ExceptionHandler();
+$exceptionHandler->setLogger($logger);
+set_exception_handler(array($exceptionHandler, 'handleException'));
+
+// Set error handler
+$errorHandler = new \WAYF\ErrorHandler();
+set_error_handler(array($errorHandler, 'handleError'));
+
+register_shutdown_function('shutdown');
+
+function shutdown() {
+    $e = error_get_last();
+    if (!is_null($e)) {
+        // Instiansiate all objects to be able to log FATAL errors
+        $jakob_config = \WAYF\Configuration::getConfig();
+        $logger = \WAYF\LoggerFactory::createInstance($jakob_config['logger']);
+        $exceptionHandler = new \WAYF\ExceptionHandler();
+        $exceptionHandler->setLogger($logger);
+        $exception = new \ErrorException($e['message'], 0, $e['type'], $e['file'], $e['line']);
+        $exceptionHandler->handleException($exception);
+    }
+}
+ */
+
+// Start session
+session_start();
+
+// Protection against session fixation attacks
+session_regenerate_id(true);
+
+// Authenticate user
+if(!isset($_SESSION['SAML'])) {
+    try {
+        $sporto = new \WAYF\SAML\SPorto($sporto_config);
+        $_SESSION['SAML'] = $sporto->authenticate();
+    } catch (Exception $e) {
+        echo $e->getMessage();
+        exit;
+    }
+}
