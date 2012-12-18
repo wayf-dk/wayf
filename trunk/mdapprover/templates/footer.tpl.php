19514d3ee57d4e72c309c2da2a5f6c0dde174ce7
jach@wayf.dk <jach@wayf.dk@f0b3050b-8f52-9014-a1fc-77610a68c25a>
2012-12-18 13:33:13 +0000

    Updated link to jQuery

--- trunk/mdapprover/templates/footer.tpl.php
+++ trunk/mdapprover/templates/footer.tpl.php
@@ -2,7 +2,7 @@
                 </div>
             </div>
         </div>
-        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script src="js/jquery-plugin/jquery.base64.min.js"></script>
         <script src="js/mdapprover.js"></script>
